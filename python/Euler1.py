19bdcfee70465df3c26a003b6a283a50737dff6f
rajesh <rajesh@rajeshpg.com>
2011-09-24 16:17:50 +0530

    script refactored

--- python/Euler1.py
+++ python/Euler1.py
@@ -1,10 +1,10 @@
 import math
 
-'''
+"""
 Add all the natural numbers below one thousand that are multiples of 3 or 5.
 eg: If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
-'''
+"""
 
 def solution():
     num = range(0, 1000)
