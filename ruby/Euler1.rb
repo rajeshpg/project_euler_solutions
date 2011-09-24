6ca1bc7789fd978be078f6a901c9d9bb1a2008fb
rajesh <rajesh@rajeshpg.com>
2011-09-24 13:51:52 +0530

    added class'ical structure

--- ruby/Euler1.rb
+++ ruby/Euler1.rb
@@ -1,7 +1,13 @@
+class Euler1
 #   Add all the natural numbers below one thousand that are multiples of 3 or 5.
 #   eg: If we list all the natural numbers below 10 that are multiples of 3 or 5,
 #   we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-  print (1..999).select{|num| ((num % 3 == 0) || (num % 5 == 0))}.inject{|a,b| a+b}, "\n"
+  def self.solution
 
+    (1..999).select{|num| ((num % 3 == 0) || (num % 5 == 0))}.inject{|a,b| a+b}
+
+  end
+
+end
 
