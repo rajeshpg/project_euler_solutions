f78481ad055c71cd40b8edd0bc0217ce818c14d4
rajesh <rajesh@rajeshpg.com>
2011-09-24 13:35:18 +0530

    changes to run as ruby script

---
+++ ruby/Euler1.rb
@@ -0,0 +1,7 @@
+#   Add all the natural numbers below one thousand that are multiples of 3 or 5.
+#   eg: If we list all the natural numbers below 10 that are multiples of 3 or 5,
+#   we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  print (1..999).select{|num| ((num % 3 == 0) || (num % 5 == 0))}.inject{|a,b| a+b}, "\n"
+
+
