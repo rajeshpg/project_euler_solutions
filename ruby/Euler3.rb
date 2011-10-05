973130b8803e0ac87f3b8d5345d8e3b04e684fb1
Rajesh <rajesh@rajeshpg.com>
2011-10-05 17:52:54 +0530

    solution for problem 3

---
+++ ruby/Euler3.rb
@@ -0,0 +1,22 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143?
+require 'prime'
+
+class Euler3
+  def self.solution
+    largestPrimeFactor(600851475143)
+  end
+
+  def self.largestPrimeFactor(num)
+    Prime.each do |prime_num|
+       break if num==prime_num
+       num /= prime_num while num % prime_num == 0
+    end
+    num
+  end
+
+end
+
+
+puts Euler3.solution
--- scala/Euler.scala
+++
@@ -1,6 +0,0 @@
-object Euler {
-  def main(args : Array[String]) : Unit = {
-    
-    println(Euler1.solution());
-  }
-}
