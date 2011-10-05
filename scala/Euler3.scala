c45c050f660d78d35f09fdb89441b0e68cddcf86
Rajesh <rajesh@rajeshpg.com>
2011-10-05 13:09:03 +0530

    removed isPrime method. refactored factorize method and renamed to largestPrimeFactor

--- scala/Euler3.scala
+++ scala/Euler3.scala
@@ -1,43 +1,21 @@
-object Euler3{
+/*
+	The prime factors of 13195 are 5, 7, 13 and 29.
 
-  solution()
+	What is the largest prime factor of the number 600851475143?
+*/
+object Euler3 extends App {
 
-  def solution(): Unit = {
-    var num = 600851475143L;
-    println(factorize(num))
-  }
+    println(largestPrimefactor(600851475143l))
 
-  def factorize(num: Long, div: Long = 2): Long = {
+  def largestPrimefactor(num: Long, div: Long = 2): Long = {
 
-    var q: Long = 0;
-    var ldiv: Long = 0;
+    if (num == div)
+      return num
+    else if (num % div == 0)
+      return largestPrimefactor(num / div)
+    else
+      return largestPrimefactor(num, div + 1)
 
-    if (num % div == 0) {
-      q = num / div;
-      if (isPrime(q))
-        return q
-      else
-        return factorize(q)
-    } else {
-      ldiv = div + 1;
-      q = num;
-      return factorize(q, ldiv)
-    }
-
-  }
-
-  def isPrime(num: Long): Boolean = {
-    val maxFactor = Math.ceil(Math.sqrt(num)).toInt;
-    if (num == 2) {
-      return true;
-    }
-
-    for (i <- 2 until maxFactor) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return true;
   }
 
 }
