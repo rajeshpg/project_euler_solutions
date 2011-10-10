1f1a9eb68cc8be6741ee69bbcc58595243b9287f
Rajesh <rajesh@rajeshpg.com>
2011-10-10 16:22:19 +0530

    solution for problem 5

---
+++ scala/Euler5.scala
@@ -0,0 +1,15 @@
+/*
+Find the difference between the sum of the squares of the first one hundred natural numbers 
+and the square of the sum.*/
+object Euler5 extends App{
+	val squareOfSumOfN: Long => Long = n => square(((n*(n+1)) / 2))
+	val sumOfSquareOfN: Long => Long = n =>  ((n * (n+1) * ((2*n)+1)) / 6)
+	val square: Long => Long = (x) => x * x
+  
+	def solution(num: Long):Long = {
+	  squareOfSumOfN(num) - sumOfSquareOfN(num) 
+	}
+	
+	println(solution(100))
+	
+}
