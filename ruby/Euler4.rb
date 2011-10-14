1f779ac443a8e802fdfe68e48a46b629fe028648
Rajesh <rajesh@rajeshpg.com>
2011-10-14 14:50:30 +0530

    solution for problem 4

---
+++ ruby/Euler4.rb
@@ -0,0 +1,30 @@
+# A palindromic number reads the same both ways. 
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+class Euler4
+  def self.largestPalindrome
+    num = 0
+    (100..1000).each do |i| 
+      (i..1000).each do |j| 
+        temp = i*j
+        if (isPalindrome?(temp.to_s) && temp > num)
+         num = temp
+        end
+      end
+    end
+    num
+  end
+  
+  def self.solution
+    puts largestPalindrome 
+  end
+
+  def self.isPalindrome?(str)
+    str.reverse == str
+  end
+
+end
+
+
+Euler3.solution
