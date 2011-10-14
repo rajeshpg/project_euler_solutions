8b3d9c8f79770fb00434ff63c36eb3f78de4ab8b
Rajesh <rajesh@rajeshpg.com>
2011-10-14 16:34:55 +0530

    solution for problem 5

---
+++ ruby/Euler5.rb
@@ -0,0 +1,28 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. 
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+class Euler5
+  def self.gcd(a,b)
+    if(b==0)
+      a
+    else
+      gcd(b, a%b)
+    end
+        
+  end
+  
+  def self.lcm(a,b)
+     return (a * (b / gcd(a, b)))
+  end
+
+  def self.solution()
+      (1..20).reduce do |result,value|
+        lcm(result,value)
+      end
+  end
+
+end
+
+
+puts Euler5.solution
