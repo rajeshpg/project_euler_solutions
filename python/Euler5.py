c87a79d515841845561afbc1083518061565ac9d
Rajesh <rajesh@rajeshpg.com>
2011-10-14 16:42:10 +0530

    solution for problem 5

---
+++ python/Euler5.py
@@ -0,0 +1,18 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+def solution():
+    pass
+                
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b, a%b)
+    
+    
+def lcm(a,b):
+    return (a * (b / gcd(a, b)))       
+
