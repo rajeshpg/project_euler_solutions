df091dfa8931f112c3c93fbd980498e594288eb2
rajesh <rajesh@rajeshpg.com>
2011-10-15 17:31:56 +0530

    solution for problem 6

---
+++ python/Euler6.py
@@ -0,0 +1,13 @@
+"""
+Find the difference between the sum of the squares of the first one hundred natural numbers
+and the square of the sum.
+"""
+
+square = lambda n: n * n
+squareOfSumOfN = lambda n: square((n*(n+1))/2)
+sumOfSquareOfN = lambda n: ((n * (n+1) * ((2*n)+1)) / 6)
+
+def solution(num):
+    return squareOfSumOfN(num) - sumOfSquareOfN(num)
+                
+solution(100)
