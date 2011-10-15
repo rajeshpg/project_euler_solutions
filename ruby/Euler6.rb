f0f3550275b420c6bf23e5d4ce9be8c88af8bccd
rajesh <rajesh@rajeshpg.com>
2011-10-15 15:03:46 +0530

    solution for problem 6

---
+++ ruby/Euler6.rb
@@ -0,0 +1,18 @@
+# Find the difference between the sum of the squares of the first one hundred natural numbers 
+# and the square of the sum.
+
+
+class Euler6
+  
+  @square = Proc.new {|n| n*n}
+  @squareOfSumOfN = Proc.new {|n| @square.call((n*(n+1))/2)}
+  @sumOfSquareOfN = Proc.new {|n| ((n * (n+1) * ((2*n)+1)) / 6)}  
+
+  def self.solution(num)
+    @squareOfSumOfN.call(num) - @sumOfSquareOfN.call(num)
+  end
+
+end
+
+
+puts Euler6.solution(100)
