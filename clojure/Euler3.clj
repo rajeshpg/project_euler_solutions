abadac9541b82e71082f73c9803bc4c1a06db847
Rajesh <rajesh@rajeshpg.com>
2011-11-16 16:09:10 +0530

    solution for problem 3

--- clojure/Euler3.clj
+++ clojure/Euler3.clj
@@ -1,12 +1,18 @@
 (ns Euler3)
 ;	The prime factors of 13195 are 5, 7, 13 and 29.
 ;	What is the largest prime factor of the number 600851475143?
-;(largest-prime-factor(rem a-num div)) (largest-prime-factor a-num div+1)
-(defn largest-prime-factor
-  ([a](largest-prime-factor a-num 2))
-  ([a div]
-    (if(== a div) num (if(zero? (mod a div)) "insert code here" ))
-  )  
+
+(defn largest-prime-factor 
+  ([num] (largest-prime-factor num 2))
+  ([num div]
+     (if (== num div)
+       num
+       (if (zero? (rem num div))
+         (recur (/ num div) div)
+         (recur num (inc div))
+        )
+      )
+    )
 )
 
-(println largest-prime-factor 600851475143)
+(println (largest-prime-factor 600851475143))
